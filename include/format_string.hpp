c3edceaaa291848ec1061b99183e568308c32be9
alexandr-1k <alexander.k.spb@gmail.com>
2025-10-20 01:26:08 +0300

    Add scan func

--- include/format_string.hpp
+++ include/format_string.hpp
@@ -1,96 +1,156 @@
 #pragma once
 
-#include <expected>
-
 #include "types.hpp"
+#include <array>
+#include <cstddef>
+#include <expected>
+#include <utility>
+#include <variant>
 
 namespace stdx::details {
-
-// Шаблонный класс для хранения форматирующей строчки и ее особенностей
-// ваш код здесь
-class format_string {
-    // ваш код здесь
+struct Placeholder {
+    struct EMPTY {};
+    struct INTEGER {};
+    struct UNSIGNED {};
+    struct STRING {};
+    using Type = std::variant<EMPTY, INTEGER, UNSIGNED, STRING>;
 };
+// Шаблонный класс для хранения форматирующей строчки и ее особенностей
+template <fixed_string str>
+class format_string {
+public:
+    // Функция для получения количества плейсхолдеров и проверки корректности формирующей строки
+    static consteval std::expected<size_t, parse_error> get_number_placeholders() {
+        constexpr size_t N = str.size();
+        if (!N)
+            return 0;
+        size_t placeholder_count = 0;
+        size_t pos = 0;
+        const size_t size = N - 1;  // -1 для игнорирования нуль-терминатора
 
-// Пользовательский литерал
-/*
-ваш код здесь
-ваш код здесь operator"" _fs()  сигнатуру также поменяйте
-{
-ваш код здесь
-}
-*/
+        while (pos < size) {
+            // Пропускаем все символы до '{'
+            if (str.data[pos] != '{') {
+                ++pos;
+                continue;
+            }
 
-// Функция для получения количества плейсхолдеров и проверки корректности формирующей строки
-// Функция закомментирована, так как еще не реализованы классы, которые она использует
-/*
-// Сделайте эту свободную функцию методом класса format_string
-template<fixed_string str>
-consteval std::expected<size_t, parse_error> get_number_placeholders() {
-    constexpr size_t N = str.size();
-    if (!N)
-        return 0;
-    size_t placeholder_count = 0;
-    size_t pos = 0;
-    const size_t size = N - 1; // -1 для игнорирования нуль-терминатора
-
-    while (pos < size) {
-        // Пропускаем все символы до '{'
-        if (str.data[pos] != '{') {
-            ++pos;
-            continue;
-        }
-
-        // Проверяем незакрытый плейсхолдер
-        if (pos + 1 >= size) {
-            return std::unexpected(parse_error{"Unclosed last placeholder"});
-        }
-
-        // Начало плейсхолдера
-        ++placeholder_count;
-        ++pos;
-
-        // Проверка спецификатора формата
-        if (str.data[pos] == '%') {
-            ++pos;
-            if (pos >= size) {
+            // Проверяем незакрытый плейсхолдер
+            if (pos + 1 >= size) {
                 return std::unexpected(parse_error{"Unclosed last placeholder"});
             }
 
-            // Проверяем допустимые спецификаторы
-            const char spec = str.data[pos];
-            constexpr char valid_specs[] = {'d', 'u', 'f', 's'};
-            bool valid = false;
+            // Начало плейсхолдера
+            ++placeholder_count;
+            ++pos;
 
-            for (const char s : valid_specs) {
-                if (spec == s) {
-                    valid = true;
-                    break;
+            // Проверка спецификатора формата
+            if (str.data[pos] == '%') {
+                ++pos;
+                if (pos >= size) {
+                    return std::unexpected(parse_error{"Unclosed last placeholder"});
                 }
+
+                // Проверяем допустимые спецификаторы
+                const char spec = str.data[pos];
+                constexpr char valid_specs[] = {'d', 'u', 'f', 's'};
+                bool valid = false;
+
+                for (const char s : valid_specs) {
+                    if (spec == s) {
+                        valid = true;
+                        break;
+                    }
+                }
+
+                if (!valid) {
+                    return std::unexpected(parse_error{"Invalid specifier."});
+                }
+                ++pos;
             }
 
-            if (!valid) {
-                return std::unexpected(parse_error{"Invalid specifier."});
+            // Проверяем закрывающую скобку
+            if (pos >= size || str.data[pos] != '}') {
+                return std::unexpected(parse_error{"\'}\' hasn't been found in appropriate place"});
             }
             ++pos;
         }
 
-        // Проверяем закрывающую скобку
-        if (pos >= size || str.data[pos] != '}') {
-            return std::unexpected(parse_error{"\'}\' hasn't been found in appropriate place"});
-        }
-        ++pos;
+        return placeholder_count;
     }
 
-    return placeholder_count;
+    static constexpr auto fmt = str;
+    static constexpr auto parsed_num_of_plhold = get_number_placeholders();
+
+    static_assert(parsed_num_of_plhold.has_value(), "Could not parse format string");
+    static constexpr size_t number_placeholders = parsed_num_of_plhold.value();
+
+    using PosArray = std::array<std::pair<std::size_t, std::size_t>, parsed_num_of_plhold.value()>;
+
+    // Функция для получения позиций плейсхолдеров
+    static consteval PosArray get_placeholder_positions() {
+        size_t pos = 0;
+        constexpr size_t N = str.size();
+        const size_t size = N - 1;
+
+        PosArray arr{};
+
+        size_t idx = 0;
+        while (pos < size) {
+            if (str.data[pos] != '{') {
+                ++pos;
+                continue;
+            }
+
+            auto start_pos = pos++;
+
+            if (str.data[pos] == '%') {
+                pos += 2;  // specificators were already validated
+            }
+
+            auto end_pos = pos++;
+            arr[idx] = std::pair(start_pos, end_pos);  // [s, e]
+            ++idx;
+        }
+        return arr;
+    }
+
+    static constexpr auto placeholder_positions = get_placeholder_positions();
+
+    template <size_t Idx>
+    static consteval bool IsEmptyPlaceholder() {
+        static_assert(Idx <= placeholder_positions.size(), "Placeholder index is out of range");
+        constexpr auto pos = placeholder_positions[Idx];
+        return pos.second - pos.first == 1;
+    }
+    template <size_t Idx>
+    static consteval bool IsSignedIntPlaceholder() {
+        static_assert(Idx <= placeholder_positions.size(), "Placeholder index is out of range");
+        constexpr auto pos = placeholder_positions[Idx];
+        constexpr char specifier = fmt.data[pos.second - 1];
+        return specifier == 'd';
+    }
+    template <size_t Idx>
+    static consteval bool IsUnsignedIntPlaceholder() {
+        static_assert(Idx <= placeholder_positions.size(), "Placeholder index is out of range");
+        constexpr auto pos = placeholder_positions[Idx];
+        constexpr char specifier = fmt.data[pos.second - 1];
+        return specifier == 'u';
+    }
+    template <size_t Idx>
+    static consteval bool IsStringPlaceholder() {
+        static_assert(Idx <= placeholder_positions.size(), "Placeholder index is out of range");
+        constexpr auto pos = placeholder_positions[Idx];
+        constexpr char specifier = fmt.data[pos.second - 1];
+        return specifier == 's';
+    }
+};
+
+// Пользовательский литерал
+
+template <fixed_string str>
+constexpr auto operator""_fs() {
+    return str;
 }
-*/
 
-// Функция для получения позиций плейсхолдеров
-
-// ваш код здесь
-void get_placeholder_positions() {  // сигнатуру тоже нужно изменить
-    // ваш код здесь
-}
-
-} // namespace stdx::details
+}  // namespace stdx::details
--- include/parse.hpp
+++ include/parse.hpp
@@ -1,45 +1,68 @@
 #pragma once
 
-#include <charconv>
-#include <concepts>
-#include <optional>
-#include <system_error>
-
 #include "format_string.hpp"
 #include "types.hpp"
+#include <charconv>
+#include <cstdint>
+#include <optional>
+#include <string_view>
+#include <system_error>
+#include <type_traits>
+#include <utility>
 
 namespace stdx::details {
+using namespace std::literals;
+
+template <typename T>
+concept AllowedType =
+    std::is_same_v<T, std::int8_t> || std::is_same_v<T, std::int16_t> || std::is_same_v<T, std::int32_t> ||
+    std::is_same_v<T, std::int64_t> || std::is_same_v<T, std::uint8_t> || std::is_same_v<T, std::uint16_t> ||
+    std::is_same_v<T, std::uint32_t> || std::is_same_v<T, std::uint64_t> || std::is_same_v<T, std::string_view> ||
+    (std::is_const_v<T> &&
+     (std::is_same_v<std::remove_cv_t<T>, std::int8_t> || std::is_same_v<std::remove_cv_t<T>, std::int16_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::int32_t> || std::is_same_v<std::remove_cv_t<T>, std::int64_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::uint8_t> || std::is_same_v<std::remove_cv_t<T>, std::uint16_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::uint32_t> || std::is_same_v<std::remove_cv_t<T>, std::uint64_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::string_view>)) ||
+    (std::is_volatile_v<T> &&
+     (std::is_same_v<std::remove_cv_t<T>, std::int8_t> || std::is_same_v<std::remove_cv_t<T>, std::int16_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::int32_t> || std::is_same_v<std::remove_cv_t<T>, std::int64_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::uint8_t> || std::is_same_v<std::remove_cv_t<T>, std::uint16_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::uint32_t> || std::is_same_v<std::remove_cv_t<T>, std::uint64_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::string_view>)) ||
+    (std::is_const_v<T> && std::is_volatile_v<T> &&
+     (std::is_same_v<std::remove_cv_t<T>, std::int8_t> || std::is_same_v<std::remove_cv_t<T>, std::int16_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::int32_t> || std::is_same_v<std::remove_cv_t<T>, std::int64_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::uint8_t> || std::is_same_v<std::remove_cv_t<T>, std::uint16_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::uint32_t> || std::is_same_v<std::remove_cv_t<T>, std::uint64_t> ||
+      std::is_same_v<std::remove_cv_t<T>, std::string_view>));
 
 // Шаблонная функция, возвращающая пару позиций в строке с исходными данными, соотвествующих I-ому плейсхолдеру
-// Функция закомментирована, так как еще не реализованы классы, которые она использует
-/*
-template<int I, format_string fmt, fixed_string source>
+template <int I, format_string fmt, fixed_string source>
 consteval auto get_current_source_for_parsing() {
     static_assert(I >= 0 && I < fmt.number_placeholders, "Invalid placeholder index");
 
-    constexpr auto to_sv = [](const auto& fs) {
-        return std::string_view(fs.data, fs.size() - 1);
-    };
+    constexpr auto to_sv = [](const auto &fs) { return std::string_view(fs.data, fs.size() - 1); };
 
     constexpr auto fmt_sv = to_sv(fmt.fmt);
     constexpr auto src_sv = to_sv(source);
-    constexpr auto& positions = fmt.placeholder_positions;
+    constexpr auto &positions = fmt.placeholder_positions;
 
     // Получаем границы текущего плейсхолдера в формате
     constexpr auto pos_i = positions[I];
     constexpr size_t fmt_start = pos_i.first, fmt_end = pos_i.second;
 
     // Находим начало в исходной строке
-    constexpr auto src_start = [&]{
+    constexpr auto src_start = [&] {
         if constexpr (I == 0) {
             return fmt_start;
         } else {
             // Находим конец предыдущего плейсхолдера в исходной строке
-            constexpr auto prev_bounds = get_current_source_for_parsing<I-1, fmt, source>();
+            constexpr auto prev_bounds = get_current_source_for_parsing<I - 1, fmt, source>();
             const auto prev_end = prev_bounds.second;
 
             // Получаем разделитель между текущим и предыдущим плейсхолдерами
-            constexpr auto prev_fmt_end = positions[I-1].second;
+            constexpr auto prev_fmt_end = positions[I - 1].second;
             constexpr auto sep = fmt_sv.substr(prev_fmt_end + 1, fmt_start - (prev_fmt_end + 1));
 
             // Ищем разделитель после предыдущего значения
@@ -49,30 +72,155 @@ consteval auto get_current_source_for_parsing() {
     }();
 
     // Находим конец в исходной строке
-    constexpr auto src_end = [&]{
+    constexpr auto src_end = [&] {
         // Получаем разделитель после текущего плейсхолдера
-        if constexpr(fmt_end == (fmt_sv.size() - 1)) {
+        if constexpr (fmt_end == (fmt_sv.size() - 1)) {
             return src_sv.size();
         }
-        constexpr auto sep = fmt_sv.substr(fmt_end + 1,
-            (I < fmt.number_placeholders - 1)
-                ? positions[I+1].first - (fmt_end + 1)
-                : fmt_sv.size() - (fmt_end + 1));
+        constexpr auto sep =
+            fmt_sv.substr(fmt_end + 1, (I < fmt.number_placeholders - 1) ? positions[I + 1].first - (fmt_end + 1)
+                                                                         : fmt_sv.size() - (fmt_end + 1));
         // Ищем разделитель после текущего значения
         constexpr auto pos = src_sv.find(sep, src_start);
         return pos != std::string_view::npos ? pos : src_sv.size();
     }();
     return std::pair{src_start, src_end};
 }
-*/
 
-// Реализуйте семейство функция parse_value
+template <typename T>
+concept SignedInt = std::is_integral_v<T> && std::is_signed_v<T>;
 
-// Шаблонная функция, выполняющая преобразования исходных данных в конкретный тип на основе I-го плейсхолдера
+template <typename T>
+concept UnsignedInt = std::is_integral_v<T> && !std::is_signed_v<T>;
 
-// здесь ваш код
-void parse_input() {  // поменяйте сигнатуру
-    // здесь ваш код
+template <typename T>
+concept AnyDigit = SignedInt<T> || UnsignedInt<T>;
+
+template <typename T>
+concept StringView = std::is_same_v<T, std::string_view>;
+
+template <AnyDigit DigitType>
+constexpr std::optional<DigitType> to_digit(std::string_view sv) {
+    DigitType result{};
+    auto [ptr, ec] = std::from_chars(sv.data(), sv.data() + sv.size(), result);
+
+    if (ec == std::errc()) {
+        return result;
+    }
+    return std::nullopt;
 }
 
-} // namespace stdx::details
+template <SignedInt RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::EMPTY) {
+    constexpr auto res = to_digit<RequestedType>(std::string_view{source.data});
+    if constexpr (!res.has_value()) {
+        // throw_static_assert<"Failed to parse signed integer at empty placeholder from: ", source.data>();
+        static_assert(false, "Failed to parse signed integer at empty placeholder");
+    }
+    return res.value();
+}
+
+template <SignedInt RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::STRING) {
+    static_assert(false, "Placeholder contains string specifier, but singed integer requested");
+}
+
+template <SignedInt RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::INTEGER) {
+    constexpr auto res = to_digit<RequestedType>(std::string_view{source.data});
+    if constexpr (!res.has_value()) {
+        static_assert(false, "Failed to parse signed integer at `d` placeholder");
+    }
+    return res.value();
+}
+
+template <SignedInt RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::UNSIGNED) {
+    constexpr auto res = to_digit<RequestedType>(std::string_view{source.data});
+    if constexpr (!res.has_value()) {
+        static_assert(false, "Failed to parse signed integer at `u` placeholder");
+    }
+    return res.value();
+}
+
+template <UnsignedInt RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::EMPTY) {
+    constexpr auto res = to_digit<RequestedType>(std::string_view{source.data});
+    if constexpr (!res.has_value()) {
+        static_assert(false, "Failed to parse unsigned integer at empty placeholder");
+    }
+    return res.value();
+}
+
+template <UnsignedInt RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::STRING) {
+    static_assert(false, "Placeholder contains string specifier, but unsigned integer requested");
+}
+
+template <UnsignedInt RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::INTEGER) {
+    constexpr auto res = to_digit<RequestedType>(std::string_view{source.data});
+    if constexpr (!res.has_value()) {
+        static_assert(false, "Failed to parse unsigned integer at `d` placeholder");
+    }
+    return res.value();
+}
+
+template <UnsignedInt RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::UNSIGNED) {
+    constexpr auto res = to_digit<RequestedType>(std::string_view{source.data});
+    if constexpr (!res.has_value()) {
+        static_assert(false, "Failed to parse unsigned integer at `u` placeholder");
+    }
+    return res.value();
+}
+
+template <StringView RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::EMPTY) {
+    return source.data;
+}
+
+template <StringView RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::STRING) {
+    return source.data;
+}
+
+template <StringView RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::INTEGER) {
+    static_assert(false, "Placeholder contains integer specifier, but string_view requested");
+}
+
+template <StringView RequestedType, fixed_string source>
+consteval RequestedType parse_value(Placeholder::UNSIGNED) {
+    static_assert(false, "Placeholder contains unsigned specifier, but string_view requested");
+}
+
+// Шаблонная функция, выполняющая преобразования исходных данных в конкретный тип на основе I-го плейсхолдера
+template <int Idx, format_string fmt, fixed_string source, AllowedType OutType>
+consteval OutType parse_input() {
+    constexpr auto source_pos = get_current_source_for_parsing<Idx, fmt, source>();
+    constexpr auto sv = std::string_view{source.data};
+    constexpr auto substr = sv.substr(source_pos.first, source_pos.second - source_pos.first);
+
+    constexpr auto beg = substr.data();
+    constexpr auto end = substr.data() + substr.size();
+
+    constexpr fixed_string<substr.size() + 1> substr_fs{beg, end};
+
+    if constexpr (fmt.template IsSignedIntPlaceholder<Idx>()) {
+        return parse_value<OutType, substr_fs>(Placeholder::INTEGER{});
+
+    } else if constexpr (fmt.template IsUnsignedIntPlaceholder<Idx>()) {
+        return parse_value<OutType, substr_fs>(Placeholder::UNSIGNED{});
+
+    } else if constexpr (fmt.template IsStringPlaceholder<Idx>()) {
+        return parse_value<OutType, substr_fs>(Placeholder::STRING{});
+
+    } else if constexpr (fmt.template IsEmptyPlaceholder<Idx>()) {
+        return parse_value<OutType, substr_fs>(Placeholder::EMPTY{});
+    } else {
+        std::unreachable();
+    }
+}
+
+}  // namespace stdx::details
